1825e1ef22d1ce189eaf3e3e67cdd520e519ad31
Kolyox <alikan2907@gmail.com>
2025-06-04 15:48:07 +0200

    ajout case a cocher et bouton effacer

--- script.js
+++ script.js
@@ -12,3 +12,52 @@ function chambremoins(){
     }
 }
 
+function adulteplus() {
+    let input = document.getElementById("adulte");
+    input.value = parseInt(input.value) + 1;
+}
+
+
+function adultemoins(){
+    let input = document.getElementById("adulte");
+    let value = parseInt(input.value);
+    if (value>0){
+        input.value-=1;
+    }
+}
+
+
+
+function enfantplus() {
+    let input = document.getElementById("enfant");
+    input.value = parseInt(input.value) + 1;
+}
+
+function enfantmoins(){
+    let input = document.getElementById("enfant");
+    let value = parseInt(input.value);
+
+    if (value>0){
+        input.value-=1;
+    }
+}
+
+
+function effacerChamps() {
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach(input => {
+    input.value = "";
+  });
+  
+  const zero = document.querySelectorAll("#enfant,#chambre,#adulte");
+    zero.forEach(input => {
+    input.value = "0";
+  });
+
+  document.getElementById("voyage").checked = false;
+}
+
+
+
+
+
